816a29b1c978bb6ac9c8de0c88dd6e667d578f39
kourosh <kourosh23@hotmail.com>
2025-02-03 13:59:18 +0100

    ApplicationDbContext

--- BlazorSportsStoreNet9/Data/ApplicationDbContext.cs
+++ BlazorSportsStoreNet9/Data/ApplicationDbContext.cs
@@ -1,9 +1,14 @@
+using BlazorSportsStoreNet9.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlazorSportsStoreNet9.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser, IdentityRole, string>(options)
     {
+        public DbSet<Person> People => Set<Person>();
+        public DbSet<Department> Departments => Set<Department>();
+        public DbSet<Location> Locations => Set<Location>();
     }
 }
